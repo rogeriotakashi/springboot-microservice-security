90afd68452a43ca5c1aa6843663c153e3f9d1f3f
rogerioh <rogerio.hirata@softvaro.com.br>
2019-12-18 16:54:56 -0200

    Add: AlunosRepository.java

---
+++ projetox-backend/src/main/java/com/roger/projetxbackend/repository/AlunosRepository.java
@@ -0,0 +1,9 @@
+package com.roger.projetxbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.roger.projetoxbackend.entity.Alunos;
+
+public interface AlunosRepository extends JpaRepository<Alunos, Long> {
+
+}

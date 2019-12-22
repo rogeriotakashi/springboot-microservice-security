ff914e2481ed7fa96aefb6b16ebd1ee3b935db8d
rogeriotakashi <rogeriotakashi@hotmail.com>
2019-12-22 11:04:59 -0300

    Add Authentication resource and Jwt Implementation

---
+++ projetox-backend/src/main/java/com/roger/projetoxbackend/models/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package com.roger.projetoxbackend.models;
+
+public class AuthenticationResponse {
+
+	private final String jwt;
+
+	public AuthenticationResponse(String jwt) {
+		super();
+		this.jwt = jwt;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
+
+	
+	
+	
+}
